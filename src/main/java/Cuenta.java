9bbd0a2d60aff7d14edab8804f3615835fcbd15b
Irene Riportella <riportellairene@gmail.com>
2022-08-14 15:06:22 +0200

    working with methods

--- src/main/java/Cuenta.java
+++ src/main/java/Cuenta.java
@@ -3,4 +3,20 @@ public class Cuenta {
     int agencia;
     int numero;
     String titular;
+
+    // Este método no retorna ningún valor
+    // (por eso usamos "void").
+    public void depositar(double valor) {
+        this.saldo = this.saldo + valor;
+    }
+
+    // Este método nos retorna un valor booleano.
+    public boolean retirar(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo = this.saldo - valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
---
+++ src/main/java/PruebaMetodos.java
@@ -0,0 +1,13 @@
+public class PruebaMetodos {
+    public static void main(String[] args) {
+        Cuenta miCuenta = new Cuenta();
+        miCuenta.saldo = 300;
+        miCuenta.depositar(200);
+        System.out.println(miCuenta.saldo);
+
+        miCuenta.retirar(100);
+        System.out.println(miCuenta.saldo);
+
+
+    }
+}
